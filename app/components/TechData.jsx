b9568eb45202982778b759a1652a825dd0341c17
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2023-09-05 09:42:10 +0100

    add prop to tech logo Images so they load faster

--- app/components/TechData.jsx
+++ app/components/TechData.jsx
@@ -16,62 +16,62 @@ export const techStack = [
     {
       id: 1,
       name: "Html",
-      image: <Image className="m-auto" src={html} width={60} height={60} key={html} alt="Html logo"/> 
+      image: <Image priority={true} className="m-auto" src={html} width={60} height={60} key={html} alt="Html logo"/> 
     },
     {
       id: 2,
       name: "Css",
-      image: <Image className="m-auto" src={css} width={60} height={60} key={css} alt="css logo"/> 
+      image: <Image priority={true} className="m-auto" src={css} width={60} height={60} key={css} alt="css logo"/> 
     },
     {
       id: 3,
       name: "Javascript",
-      image: <Image className="m-auto" src={javascript} width={60} height={60} key={javascript} alt="javascript logo"/>
+      image: <Image priority={true} className="m-auto" src={javascript} width={60} height={60} key={javascript} alt="javascript logo"/>
     },
     {
       id: 4,
       name: "React",
-      image: <Image className="m-auto" src={react} width={60} height={60} key={react} alt="react logo"/> 
+      image: <Image priority={true} className="m-auto" src={react} width={60} height={60} key={react} alt="react logo"/> 
     },
     {
       id: 6,
       name: "Next",
-      image: <Image className="m-auto" src={nextjs} width={60} height={60} key={nextjs} alt="NextJs logo"/>
+      image: <Image priority={true} className="m-auto" src={nextjs} width={60} height={60} key={nextjs} alt="NextJs logo"/>
     },
     {
       id: 7,
       name: "Tailwind",
-      image: <Image  className="m-auto" src={tailwind} width={60} height={60} key={tailwind} alt="Tailwind logo"/>
+      image: <Image priority={true}  className="m-auto" src={tailwind} width={60} height={60} key={tailwind} alt="Tailwind logo"/>
     },
     {
       id: 8,
       name: "Node",
-      image: <Image className="m-auto" src={node} key={node} width={60} height={60} alt="Node logo"/>
+      image: <Image priority={true} className="m-auto" src={node} key={node} width={60} height={60} alt="Node logo"/>
     },
     {
       id: 9,
       name: "Express",
-      image: <Image className="m-auto" width={60} height={60} src={express} key={express} alt="Express logo"/>
+      image: <Image priority={true} className="m-auto" width={60} height={60} src={express} key={express} alt="Express logo"/>
     },
     {
       id: 10,
       name: "Firebase",
-      image: <Image className="m-auto" width={60} height={60} src={firebase} key={firebase} alt="Firebase logo"/>
+      image: <Image  priority={true}className="m-auto" width={60} height={60} src={firebase} key={firebase} alt="Firebase logo"/>
     },
     {
       id: 11,
       name: "MongoDB",
-      image: <Image className="m-auto" width={60} height={60} src={mongodb} key={mongodb} alt="MongoDb logo"/>
+      image: <Image priority={true} className="m-auto" width={60} height={60} src={mongodb} key={mongodb} alt="MongoDb logo"/>
     },
     {
       id: 12,
       name: "PostGres",
-      image: <Image className="m-auto" src={postgres} key={postgres} width={60} height={60} alt="Postgres logo"/>
+      image: <Image priority={true} className="m-auto" src={postgres} key={postgres} width={60} height={60} alt="Postgres logo"/>
     },
     {
       id: 13,
       name: "Jest",
-      image: <Image className="m-auto" width={60} height={60} src={jest} key={jest} alt="Html logo"/>
+      image: <Image priority={true} className="m-auto" width={60} height={60} src={jest} key={jest} alt="Html logo"/>
     },
   
   ]
